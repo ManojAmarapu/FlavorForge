d77fea616b2c15700477156e1e5973a1ed62e10f
Manoj kumar <manojamarapu@gmail.com>
2026-03-01 22:41:52 +0530

    Final Identity Fix: Prioritize Frontend ID over MongoDB _id to Prevent Favorites Desync

--- src/utils/normalizeRecipeId.ts
+++ src/utils/normalizeRecipeId.ts
@@ -2,6 +2,6 @@ import { Recipe } from '../types/recipe';
 
 export const getCanonicalId = (recipe: Recipe | any): string => {
     if (!recipe) return '';
-    const id = recipe._id ?? recipe.id ?? recipe.recipeId;
+    const id = recipe.id ?? recipe.recipeId ?? recipe._id;
     return id ? id.toString() : '';
 };
