e40bd130b66fc1d5bfef3509c58582e5cbe35db2
Manoj kumar <manojamarapu@gmail.com>
2026-03-01 15:58:38 +0530

    Final Polish: Ultra-smooth background floating animation refinement

--- src/components/CulinaryBackground.tsx
+++ src/components/CulinaryBackground.tsx
@@ -1,126 +1,52 @@
 import { motion } from "framer-motion";
 
 export default function CulinaryBackground() {
+    const backgroundItems = [
+        { emoji: '🍅', top: '10%', left: '8%', duration: 25 },
+        { emoji: '🥒', top: '8%', left: '45%', duration: 28 },
+        { emoji: '🥬', top: '15%', right: '12%', duration: 30 },
+        { emoji: '🥕', top: '60%', left: '30%', duration: 32 },
+        { emoji: '🔪', top: '40%', right: '18%', duration: 34, baseRotate: 12 },
+        { emoji: '🧄', bottom: '35%', left: '8%', duration: 36 },
+        { emoji: '🥔', bottom: '45%', right: '8%', duration: 38 },
+        { emoji: '🧅', bottom: '10%', left: '25%', duration: 40 },
+        { emoji: '🍄', bottom: '5%', left: '54%', duration: 27 },
+        { emoji: '🍳', bottom: '12%', right: '20%', duration: 31 },
+        { emoji: '🫑', top: '30%', left: '20%', duration: 33 },
+        { emoji: '🥦', bottom: '25%', right: '35%', duration: 37 },
+        { emoji: '🌶️', top: '32%', left: '56%', duration: 41 },
+    ];
+
     return (
         <div className="fixed inset-0 pointer-events-none select-none z-0">
-
-            {/* Top-left */}
-            <motion.div
-                className="absolute top-[10%] left-[8%] text-7xl opacity-40"
-                animate={{ y: [0, -35, 0], x: [0, 10, 0] }}
-                transition={{ duration: 18, repeat: Infinity, ease: "easeInOut" }}
-            >
-                🍅
-            </motion.div>
-
-            {/* Top-mid */}
-            <motion.div
-                className="absolute top-[8%] left-[45%] text-7xl opacity-40"
-                animate={{ y: [0, -35, 0], x: [0, 10, 0] }}
-                transition={{ duration: 20, repeat: Infinity, ease: "easeInOut" }}
-            >
-                🥒
-            </motion.div>
-
-            {/* Top-right */}
-            <motion.div
-                className="absolute top-[15%] right-[12%] text-7xl opacity-40"
-                animate={{ y: [0, -35, 0], x: [0, 10, 0] }}
-                transition={{ duration: 17, repeat: Infinity, ease: "easeInOut" }}
-            >
-                🥬
-            </motion.div>
-
-            {/* Behind 'Discover recipes' text (Carrot) */}
-            <motion.div
-                className="absolute top-[60%] left-[30%] text-7xl opacity-40"
-                animate={{ y: [0, -35, 0], x: [0, 10, 0] }}
-                transition={{ duration: 19, repeat: Infinity, ease: "easeInOut" }}
-            >
-                🥕
-            </motion.div>
-
-            {/* Mid-right */}
-            <motion.div
-                className="absolute top-[40%] right-[18%] text-7xl opacity-40 rotate-12"
-                animate={{ y: [0, -35, 0], x: [0, 10, 0] }}
-                transition={{ duration: 22, repeat: Infinity, ease: "easeInOut" }}
-            >
-                🔪
-            </motion.div>
-
-            {/* Lower Mid-left */}
-            <motion.div
-                className="absolute bottom-[35%] left-[8%] text-7xl opacity-40"
-                animate={{ y: [0, -35, 0], x: [0, 10, 0] }}
-                transition={{ duration: 16, repeat: Infinity, ease: "easeInOut" }}
-            >
-                🧄
-            </motion.div>
-
-            {/* Lower Mid-right */}
-            <motion.div
-                className="absolute bottom-[45%] right-[8%] text-7xl opacity-40"
-                animate={{ y: [0, -35, 0], x: [0, 10, 0] }}
-                transition={{ duration: 21, repeat: Infinity, ease: "easeInOut" }}
-            >
-                🥔
-            </motion.div>
-
-            {/* Bottom-left */}
-            <motion.div
-                className="absolute bottom-[10%] left-[25%] text-7xl opacity-40"
-                animate={{ y: [0, -35, 0], x: [0, 10, 0] }}
-                transition={{ duration: 20, repeat: Infinity, ease: "easeInOut" }}
-            >
-                🧅
-            </motion.div>
-
-            {/* Below 'Start Cooking' button, slight right (Mushroom) safely lowered */}
-            <motion.div
-                className="absolute bottom-[5%] left-[54%] text-7xl opacity-40"
-                animate={{ y: [0, -35, 0], x: [0, 10, 0] }}
-                transition={{ duration: 19, repeat: Infinity, ease: "easeInOut" }}
-            >
-                🍄
-            </motion.div>
-
-            {/* Bottom-right */}
-            <motion.div
-                className="absolute bottom-[12%] right-[20%] text-7xl opacity-40"
-                animate={{ y: [0, -35, 0], x: [0, 10, 0] }}
-                transition={{ duration: 19, repeat: Infinity, ease: "easeInOut" }}
-            >
-                🍳
-            </motion.div>
-
-            {/* Scattered Extra 1 */}
-            <motion.div
-                className="absolute top-[30%] left-[20%] text-7xl opacity-40"
-                animate={{ y: [0, -35, 0], x: [0, 10, 0] }}
-                transition={{ duration: 17, repeat: Infinity, ease: "easeInOut" }}
-            >
-                🫑
-            </motion.div>
-
-            {/* Scattered Extra 2 */}
-            <motion.div
-                className="absolute bottom-[25%] right-[35%] text-7xl opacity-40"
-                animate={{ y: [0, -35, 0], x: [0, 10, 0] }}
-                transition={{ duration: 22, repeat: Infinity, ease: "easeInOut" }}
-            >
-                🥦
-            </motion.div>
-
-            {/* Above last 'e' in FlavorForge (Chili) */}
-            <motion.div
-                className="absolute top-[32%] left-[56%] text-7xl opacity-40"
-                animate={{ y: [0, -35, 0], x: [0, 10, 0] }}
-                transition={{ duration: 16, repeat: Infinity, ease: "easeInOut" }}
-            >
-                🌶️
-            </motion.div>
-
+            {backgroundItems.map((item, index) => {
+                const baseRotate = item.baseRotate || 0;
+                return (
+                    <motion.div
+                        key={index}
+                        className="absolute text-7xl opacity-40"
+                        style={{
+                            ...(item.top && { top: item.top }),
+                            ...(item.bottom && { bottom: item.bottom }),
+                            ...(item.left && { left: item.left }),
+                            ...(item.right && { right: item.right }),
+                            willChange: "transform"
+                        }}
+                        animate={{
+                            y: [0, -20, 10, 0],
+                            x: [0, 10, -10, 0],
+                            rotate: [baseRotate, baseRotate + 3, baseRotate - 3, baseRotate]
+                        }}
+                        transition={{
+                            duration: item.duration,
+                            repeat: Infinity,
+                            ease: "easeInOut"
+                        }}
+                    >
+                        {item.emoji}
+                    </motion.div>
+                );
+            })}
         </div>
     );
 }
